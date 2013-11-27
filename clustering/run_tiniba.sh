125f548fad2f37580cd6749efef26bb5756bd2a4
Sean Anderson <sean.martin.anderson@gmail.com>
2013-11-26 18:36:03 -0600

    trying to get rid of "compita" bullshit

--- clustering/run_tiniba.sh
+++ clustering/run_tiniba.sh
@@ -238,8 +238,8 @@ ecut=`grep ecut setUpAbinit_$case.in  |  awk '{print $2}'`
 if [ -e $case'_scf'/$case.out ]
     then
     ecutout=`grep "ecut(hartree)" $case'_scf'/$case.out | awk '{print $2}'`
-    ecutyn=`echo $ecut $ecutout | $where/$compita`
-    if [ $ecutyn == 'no' ]
+    #ecutyn=`echo $ecut $ecutout | $where/$compita`
+    if [ $ecut -ne $ecutout ]
 	then
 	Line
 	echo -e ${RED} ecut=$ecut at setUpAbinit_$case.in different from ecut=$ecutout at $case'_scf'/$case.out ${NC}
