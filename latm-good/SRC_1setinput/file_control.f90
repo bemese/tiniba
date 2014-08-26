11c9e8c6267e441ad9defee8a57c4aeed019def4
Sean Anderson <sean.martin.anderson@gmail.com>
2014-08-26 08:15:49 -0500

    added latm

---
+++ latm-good/SRC_1setinput/file_control.f90
@@ -0,0 +1,95 @@
+MODULE file_control
+  USE inparams, ONLY : number_of_spectra_to_calculate, spectrum_info
+  USE constants, ONLY : debug
+!!!    TYPE spectrum
+!!!     CHARACTER(LEN=80) :: integrand_filename
+!!!     INTEGER :: integrand_filename_unit
+!!!     INTEGER :: spectrum_type
+!!!     !  1: chi1          !  2: Lambda        !  3: Gamma         ! 4: S
+!!!     !  5: C             !  6: Ctilde        !  7: E             ! 8: Etilde
+!!!     !  9: staticChi1    ! 10: staticS       ! 11: staticC       ! 12: staticCtilde
+!!!     ! 13: staticE       ! 14: staticEtilde  ! 15: staticChi2i   ! 16: staticChi2e
+!!!     ! 17:               ! 18:               ! 19:               ! 20:
+!!!     ! 21:               ! 22:               ! 23:               ! 24:
+!!!     LOGICAL :: compute_integrand
+!!!     INTEGER, POINTER :: spectrum_tensor_component(:)
+!!!     REAL(DP), POINTER :: transformation_elements(:)
+!!!  END TYPE spectrum
+  IMPLICIT NONE
+  INTEGER :: i
+CONTAINS
+  !==================================================================
+  SUBROUTINE openOutputDataFiles
+    IMPLICIT NONE
+    INTEGER :: istat
+    
+    DO i=1,number_of_spectra_to_calculate
+       IF (spectrum_info(i)%compute_integrand) THEN
+          if(debug)WRITE(6,*) "Trying to open: ", spectrum_info(i)%integrand_filename
+          OPEN(UNIT = spectrum_info(i)%integrand_filename_unit,&
+               FILE = spectrum_info(i)%integrand_filename, &
+               IOSTAT = istat)
+          IF (istat/=0) THEN
+             WRITE(6,*) ' '
+             WRITE(6,*) 'Error opening', spectrum_info(i)%integrand_filename
+             WRITE(6,*) 'Assigned unit', spectrum_info(i)%integrand_filename_unit
+             WRITE(6,*) 'file_control.f90: STOPPING2'
+             STOP
+!          ELSE IF (istat.EQ.0) THEN
+!             if(debug) WRITE(6,*) 'File opened'
+          END IF
+       END IF
+    END DO
+  END SUBROUTINE openOutputDataFiles
+  !==================================================================
+  
+  !==================================================================
+  SUBROUTINE closeOutputDataFiles
+    IMPLICIT NONE
+    INTEGER :: istat
+    
+    DO i=1,number_of_spectra_to_calculate
+       IF (spectrum_info(i)%compute_integrand) THEN
+          CLOSE(spectrum_info(i)%integrand_filename_unit, IOSTAT = istat)
+          IF (istat.EQ.0) THEN
+             if(debug) WRITE(6,*) 'Closing files'
+          ELSE
+             WRITE(6,*) ' '
+             WRITE(6,*) 'Error closing', spectrum_info(i)%integrand_filename
+             WRITE(6,*) 'Assigned unit', spectrum_info(i)%integrand_filename_unit
+             WRITE(6,*) 'file_control.f90: STOPPING1'
+             STOP
+          END IF
+       END IF
+    END DO
+  END SUBROUTINE closeOutputDataFiles
+  !==================================================================
+  
+  SUBROUTINE writeOutputDataFileHeaders
+    USE INPARAMS, ONLY : DP, spin_factor, spectrum_factor
+    USE INPARAMS, ONLY : nSym, unit_cell_volume
+    IMPLICIT NONE
+    REAL(DP) :: rtmp, rtmp2
+    
+    IF (debug) WRITE(*,*) "Program Flow: entered file_control.f90"
+    
+!!!    rtmp = spin_factor*(1.d0/unit_cell_volume)*(1.d0/DFLOAT(2*nSym))
+!    rtmp = spin_factor*(1.d0/DFLOAT(2*nSym))
+    rtmp = spin_factor*(1.d0/DFLOAT(nSym))
+    
+    
+    DO i=1,number_of_spectra_to_calculate
+       IF (spectrum_info(i)%compute_integrand) THEN
+          rtmp2 = rtmp*spectrum_factor(spectrum_info(i)%spectrum_type)
+          if(debug) WRITE (6,*) "writeOutputDataFileHeaders", i
+          if(debug) WRITE (6,*) "response-prefactor", rtmp2
+          IF (debug) WRITE(*,*) "Program Flow: out of file_control.f90"
+          WRITE(UNIT=spectrum_info(i)%integrand_filename_unit, FMT=*) rtmp2
+       END IF
+    END DO
+
+
+    
+  END SUBROUTINE writeOutputDataFileHeaders
+  
+END MODULE file_control
