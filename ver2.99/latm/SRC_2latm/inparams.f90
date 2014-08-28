9f9e2a2c776755d0441cf27b14ce1c0cc72081b6
Sean Anderson <sean.martin.anderson@gmail.com>
2014-08-28 15:08:34 -0500

    removed previous bad versions of tiniba

--- ver2.99/latm/SRC_2latm/inparams.f90
+++
@@ -1,1 +0,0 @@
-../SRC_1setinput/inparams.f90
--- ver3.0/utils/try.sh
+++
@@ -1,7 +0,0 @@
-#($(<hoy))
-#< hoy
-source hoy
-printf "\t$ab_exec_hexa\n"
-printf "\t$ab_exec_quad\n"
-printf "\t$ab_exec_itanium\n"
-printf "\t$ab_exec_xeon\n"
